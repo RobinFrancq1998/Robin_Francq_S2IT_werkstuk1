46e8894ce7462178346e70fa32fc50803c486d34
RobinFrancq1998 <32958838+RobinFrancq1998@users.noreply.github.com>
2018-05-06 17:20:35 +0200

    aanmaak project

---
+++ Robin_Francq_S2IT_werkstuk1/Robin_Francq_S2IT_werkstuk1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Robin_Francq_S2IT_werkstuk1
+//
+//  Created by Robin Francq on 6/05/18.
+//  Copyright © 2018 Robin Francq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
