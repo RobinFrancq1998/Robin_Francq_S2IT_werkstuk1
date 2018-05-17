aa85199ee55bf345c6952bace122bca248d0d2c3
RobinFrancq1998 <robin.francq@outlook.com>
2018-05-17 11:44:19 +0200

    Toevoegen van map view in tab 2

--- Robin_Francq_S2IT_werkstuk1/Robin_Francq_S2IT_werkstuk1/ViewController2.swift
+++ Robin_Francq_S2IT_werkstuk1/Robin_Francq_S2IT_werkstuk1/ViewController2.swift
@@ -9,12 +9,25 @@
 import UIKit
 import MapKit
 
-class ViewController2: UIViewController {
+class ViewController2: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var map: MKMapView!
+    
+    var manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        manager.requestAlwaysAuthorization()
+        
+        manager.startUpdatingLocation()
+        
+        let location = CLLocationCoordinate2D(latitude: 50.852183, longitude: 4.343737)
+        let myAnnotation = MyAnnotation(coordinate: location, title: "Barbeton")
+        
+        self.map.addAnnotation(myAnnotation)
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,15 +35,10 @@ class ViewController2: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 60, longitudeDelta: 60)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
     }
-    */
-
 }
